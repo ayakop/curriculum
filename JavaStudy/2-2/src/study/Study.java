f8277074c07ef010498c68e837a9aed916fd62a0
ayakop <ayako.yanagi96@gmail.com>
2021-03-03 16:19:21 +0900

    【提出課題】mainメソッド・インスタンス・メソッドの呼び出し
    Study.java, Main.javaの作成

---
+++ JavaStudy/2-2/src/study/Study.java
@@ -0,0 +1,39 @@
+package study;
+
+
+/**
+ * Study.java
+ */
+class Account { 
+
+    // フィールド変数
+    private String userName;
+    private int id;
+    private String password;
+
+    Account() {
+       userName = "hoge";
+       id = 123;
+       password = "abc123";
+    }
+    
+    // コンストラクタ
+    public Account(String userName, int id, String password) {
+        this.userName = userName;
+        this.id = id;
+        this.password = password;
+    }
+
+ 
+    
+
+    // ① 以下のルールに従いアカウント情報（名前、ID、パスワード）を出力するメソッドを作成してください。
+    // アクセス修飾子:「protected」
+    // メソッド名:「printAccountInfo」
+    protected void printAccountInfo() {
+        System.out.println("ユーザー名は、 " + this.userName);
+        System.out.println("IDは、" + this.id);
+        System.out.println("パスワードは、" + this.password);
+        }
+
+}
