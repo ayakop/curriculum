0fe333caaf27666d6f43a8bfb0ca462ddb1885fa
ayakop <ayako.yanagi96@gmail.com>
2021-03-05 23:49:12 +0900

    【再提出】mainメソッド・インスタンス・メソッドの呼び出し
    Main.javaの修正

---
+++ JavaStudy/2-3/src/study/A.java
@@ -0,0 +1,11 @@
+package study;
+
+public class A {
+    private String name;
+    public void setName(String Name) {
+        this.name = Name;
+    }
+    public  String getNameString() {
+        return this.name;
+    }
+}
---
+++ JavaStudy/2-3/src/study/Main.java
@@ -0,0 +1,11 @@
+package study;
+
+public class Main {
+
+    A a = new A;
+    a.setName("A");
+    System.out.println(a.getName);
+
+
+
+}
